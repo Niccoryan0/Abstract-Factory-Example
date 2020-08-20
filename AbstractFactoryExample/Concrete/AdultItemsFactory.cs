beaed4f855ca5af599b2f1040dd96c6a3fbb144e
Nicco Ryan <niccoryan0@gmail.com>
2020-08-19 20:49:24 -0700

    Add project files.

---
+++ AbstractFactoryExample/Concrete/AdultItemsFactory.cs
@@ -0,0 +1,23 @@
+﻿using AbstractFactoryExample.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactoryExample.Concrete
+{
+    public class AdultItemsFactory : RestaruantFactory
+    {
+        public Sandwich MakeSandwich()
+        {
+            return new Burger();
+        }
+
+        Drink RestaruantFactory.MakeDrink()
+        {
+            return new Beer();
+        }
+    }
+    public class Burger : Sandwich { }
+
+    public class Beer : Drink { }
+}
